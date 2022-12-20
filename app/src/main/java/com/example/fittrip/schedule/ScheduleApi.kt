35ab00aa6806fff7c64895729baed0fa168f1511
PPakSang <sanghyun-dev@naver.com>
2022-12-20 15:35:21 +0900

    Feat: Share Schedule 관련 API 추가

--- app/src/main/java/com/example/fittrip/schedule/ScheduleApi.kt
+++ app/src/main/java/com/example/fittrip/schedule/ScheduleApi.kt
@@ -5,6 +5,8 @@ import retrofit2.http.Body
 import retrofit2.http.GET
 import retrofit2.http.Header
 import retrofit2.http.POST
+import retrofit2.http.PUT
+import retrofit2.http.Query
 
 interface ScheduleApi {
     companion object {
@@ -14,16 +16,50 @@ interface ScheduleApi {
     @GET("/api/v1/schedules")
     fun getMySchedule(
         @Header("Authorization") token: String,
-    ): retrofit2.Call<MutableList<MyScheduleResponse>>
+    ): retrofit2.Call<MutableList<ScheduleResponse>>
 
     @POST("/api/v1/schedules")
     fun createSchedule(
         @Header("Authorization") token: String,
         @Body req: CreateScheduleRequest
     ): retrofit2.Call<Unit>
+
+    // ### Schedule Share ###
+
+    @PUT("/api/v1/route")
+    fun changeShareStatus(
+        @Header("Authorization") token: String,
+        @Body req: ShareScheduleRequest
+    ): retrofit2.Call<Unit>
+
+    @GET("/api/v1/schedules/shared")
+    fun getAllSharedSchedule(): retrofit2.Call<ScheduleResponse>
+
+    // ### Schedule Detail ###
+
+    @GET("/api/v1/route")
+    fun getRouteInfoByScheduleId(
+        @Header("Authorization") token: String,
+        @Query("scheduleId") scheduleId: Int
+    ): retrofit2.Call<RouteInfoResponseDto>
 }
 
-data class MyScheduleResponse(
+data class RouteInfoResponseDto(
+    val duration: RouteInfoPerDurationDto,
+    val fare: RouteInfoPerFareDto
+)
+
+data class RouteInfoPerDurationDto(
+    val locations: List<LocationDto>,
+    val durationForNextPlace: List<Int>
+)
+
+data class RouteInfoPerFareDto(
+    val locations: List<LocationDto>,
+    val fareForNextPlace: List<Int>
+)
+
+data class ScheduleResponse(
     var id: Int,
     var name: String,
     var totalDuration: Int,
@@ -36,4 +72,9 @@ data class MyScheduleResponse(
 data class CreateScheduleRequest(
     var locations: List<LocationDto>,
     var name: String = "새로운 일정"
+)
+
+data class ShareScheduleRequest(
+    var scheduleId: Int,
+    var sharedStatus: Boolean
 )
