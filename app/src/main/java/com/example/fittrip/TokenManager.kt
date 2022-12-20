afa75ee7368b438ff2c21e448a1a7734b5ed00ee
HyungUk Ryu <hw0603@naver.com>
2022-12-20 18:40:26 +0900

    Feat: User 로그인 기능 추가

--- app/src/main/java/com/example/fittrip/TokenManager.kt
+++ app/src/main/java/com/example/fittrip/TokenManager.kt
@@ -2,6 +2,6 @@ package com.example.fittrip
 
 class TokenManager {
     companion object {
-        val token = "eyJ0eXAiOiJKV1QiLCJhbGciOiJIUzI1NiJ9.eyJpc3MiOiJtYXBwIiwiaWQiOiJ0ZXN0IiwiZXhwIjoxNjcyMTU1ODQyLCJ0eXBlIjoiYWNjZXNzIiwiaWF0IjoxNjcxNDM1ODQyfQ.rvgmAWfJYH5Q-WpjHsBvBrTuAyBL0QY4Rt-KnxKXoy0"
+        var token = "eyJ0eXAiOiJKV1QiLCJhbGciOiJIUzI1NiJ9.eyJpc3MiOiJtYXBwIiwiaWQiOiJ0ZXN0IiwiZXhwIjoxNjcyMTU1ODQyLCJ0eXBlIjoiYWNjZXNzIiwiaWF0IjoxNjcxNDM1ODQyfQ.rvgmAWfJYH5Q-WpjHsBvBrTuAyBL0QY4Rt-KnxKXoy0"
     }
 }
---
+++ app/src/main/java/com/example/fittrip/login/LoginApi.kt
@@ -0,0 +1,26 @@
+package com.example.fittrip.login
+
+import retrofit2.http.Body
+import retrofit2.http.POST
+import retrofit2.http.Query
+
+interface LoginApi {
+    companion object {
+        const val BASE_URL = "http://10.0.2.2:8080"
+    }
+
+    @POST("/api/v1/auth")
+    fun login(
+        @Body req: LoginRequestDto
+    ): retrofit2.Call<LoginResponseDto>
+}
+
+data class LoginResponseDto(
+    val accessToken: String
+)
+
+data class LoginRequestDto(
+    val id: String,
+    val name: String = "test",
+    val password: String
+)
